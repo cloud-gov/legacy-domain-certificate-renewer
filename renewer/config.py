b515ed8fd9238e545f6cb02574121c8c5921e41d
Ben Berry <ben.berry@gsa.gov>
2021-08-26 15:51:43 -0700

    set aws vars on schema config so we can import the things we need

--- renewer/config.py
+++ renewer/config.py
@@ -75,3 +75,6 @@ class UpgradeSchemaConfig(Config):
         self.CDN_BROKER_DATABASE_URI = normalize_db_url(cdn_db.credentials["uri"])
         alb_db = self.cf_env_parser.get_service(name="rds-domain-broker")
         self.DOMAIN_BROKER_DATABASE_URI = normalize_db_url(alb_db.credentials["uri"])
+        self.AWS_GOVCLOUD_REGION = None
+        self.AWS_GOVCLOUD_ACCESS_KEY_ID = None
+        self.AWS_GOVCLOUD_SECRET_ACCESS_KEY = None
