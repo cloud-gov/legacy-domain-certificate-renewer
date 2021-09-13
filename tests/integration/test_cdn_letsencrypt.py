7990978baafd76588c856816d292a5adf1997b86
Ben Berry <ben.berry@gsa.gov>
2021-09-13 16:56:05 -0700

    Get challenges for domains
    initiate challenges
    add challenge models
    add order_json to route

--- tests/integration/test_cdn_letsencrypt.py
+++ tests/integration/test_cdn_letsencrypt.py
@@ -37,3 +37,22 @@ def test_create_private_key(clean_db, cdn_route: CdnRoute, immediate_huey):
     assert certificate.csr_pem is not None
     assert "BEGIN PRIVATE KEY" in certificate.private_key_pem
     assert "BEGIN CERTIFICATE REQUEST" in certificate.csr_pem
+
+
+def test_gets_new_challenges(clean_db, cdn_route: CdnRoute, immediate_huey):
+
+    instance_id = cdn_route.instance_id
+    operation = cdn_route.create_renewal_operation()
+    clean_db.add(cdn_route)
+    clean_db.add(operation)
+    clean_db.commit()
+    operation_id = operation.id
+    letsencrypt.create_user(operation.id, cdn_route.route_type)
+    letsencrypt.create_private_key_and_csr(operation.id, cdn_route.route_type)
+
+    letsencrypt.initiate_challenges(operation.id, cdn_route.route_type)
+    clean_db.expunge_all()
+    operation = clean_db.query(CdnOperation).get(operation_id)
+    certificate = operation.certificate
+    assert certificate.challenges.count() == 2
+    assert certificate.order_json is not None
--- tests/lib/alb_fixtures.py
+++ tests/lib/alb_fixtures.py
@@ -20,7 +20,7 @@ def alb_route(clean_db, proxy):
     route = DomainRoute()
     route.instance_id = "fixture-route"
     route.state = "provisioned"
-    route.domains = "example.com"
+    route.domains = ["example.com", "www.example.com"]
     route.alb_proxy = proxy
     clean_db.add(route)
     clean_db.commit()
