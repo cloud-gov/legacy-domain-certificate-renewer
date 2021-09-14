448ba6a5fb0255727f05bf90fa8e62647f3123f5
Ben Berry <ben.berry@gsa.gov>
2021-09-14 11:54:32 -0700

    add dedicated huey consumer module

---
+++ renewer/consumer.py
@@ -0,0 +1,10 @@
+"""
+This is essentially Huey's entrypoint. We need to import:
+- our huey instance
+- any cron tasks we want to run
+we do this in a file separate from renewer.huey to avoid circular imports
+(e.g. we'd have to import renewer.tasks.cron into renewer.huey, but cron
+imports renewer.huey)
+"""
+from renewer.huey import huey
+from renewer.tasks import cron
