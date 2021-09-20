223a964defb8779c3d499778b55570b3c5ae73c0
Ben Berry <ben.berry@gsa.gov>
2021-09-20 11:31:45 -0700

    don't commit Nones

--- renewer/tasks/migrations.py
+++ renewer/tasks/migrations.py
@@ -1,9 +1,12 @@
+import logging
+
 from huey import crontab
 
 from renewer.db import SessionHandler
-
 from renewer.domain_models import find_active_instances, DomainRoute
 from renewer import huey
+
+logger = logging.getLogger(__name__)
 
 
 @huey.huey.periodic_task(crontab(month="*", day="*", hour="6", minute="0"))
@@ -15,7 +18,10 @@ def backport_all_manual_certs():
 
 @huey.nonretriable_task
 def backport_cert(session, instance_id):
+    logger.info(f"backporting cert for {instance_id}")
     instance = session.query(DomainRoute).get(instance_id)
     cert = instance.backport_manual_certs()
-    session.add(cert)
-    session.commit()
+    if cert is not None:
+        session.add(cert)
+        session.commit()
+        logger.info(f"backported cert for {instance_id}")
--- tests/integration/test_alb_backport.py
+++ tests/integration/test_alb_backport.py
@@ -50,3 +50,35 @@ def test_backport_all_certs(clean_db, alb, iam_govcloud, immediate_huey):
     )
     assert cert.expires is not None
     assert cert.name == "cf-domains-renew-me-2021-03-12_12-34-12"
+
+
+def test_no_error_when_no_backport(clean_db, alb, iam_govcloud, immediate_huey):
+    # make a route
+    proxy = DomainAlbProxy()
+    proxy.alb_arn = "arn:aws:alb:123"
+    proxy.alb_dns_name = "example.com"
+    proxy.listener_arn = "arn:aws:listener:123"
+    route = DomainRoute()
+    route.state = "provisioned"
+    route.instance_id = "renew-me"
+    route.alb_proxy_arn = "arn:aws:alb:123"
+    old_cert = DomainCertificate()
+    old_cert.route = route
+    old_cert.expires = datetime(year=2021, month=2, day=1, hour=0, minute=0, second=0)
+    old_cert.arn = "arn:aws:iam:1234:server-certificate/domains/local/cf-domains-renew-me-2021-01-01_12-34-56"
+    clean_db.add(proxy)
+    clean_db.add(route)
+    clean_db.add(old_cert)
+    clean_db.commit()
+
+    # get the certs for the alb
+    alb.expect_get_certificates_for_listener(
+        "arn:aws:listener:123",
+        7,
+        "arn:aws:iam:1234:server-certificate/domains/local/cf-domains-renew-me-2021-01-01_12-34-56",
+    )
+    clean_db.expunge_all()
+
+    # function we're actually testing
+    # the test here is just that it doesn't blow up
+    migrations.backport_cert("renew-me")
